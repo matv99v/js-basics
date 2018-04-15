6810e153e772f22851d249246ac57b837cfd4ccc
vmatveev <vmatveev.dataart.com>
2018-04-15 15:15:29 +0300

    XHR and form post

---
+++ public/app.js
@@ -0,0 +1,132 @@
+// helpers
+
+function processResponse() {
+    if (this.readyState != 4) return; // process only the last state - DONE
+    if (this.status == 200) {
+        alert(this.responseText);
+    } else {
+        alert(this.status + ': ' + this.statusText);
+    }
+}
+
+function getFormData(formEl) {
+    const formData = new FormData(formEl);
+    const data = {};
+    for (const pair of formData.entries()) {
+        data[pair[0]] = pair[1];
+    }
+    return data;
+}
+
+function buildQueryString(obj) {
+    return Object.keys(obj)
+        .map(el => encodeURIComponent(el) + '=' + encodeURIComponent(obj[el]))
+        .join('&');
+}
+
+function buildMultipartObj(obj) {
+    const boundary = String(Math.random()).slice(2);
+    const boundaryMiddle = '--' + boundary + '\r\n';
+    const boundaryLast = '--' + boundary + '--\r\n'
+    const body = ['\r\n'];
+    for (var key in obj) {
+      body.push('Content-Disposition: form-data; name="' + key + '"\r\n\r\n' + obj[key] + '\r\n');
+    }
+    return {
+        body: body.join(boundaryMiddle) + boundaryLast,
+        boundary: boundary
+    };
+}
+
+// request handlers
+
+function getUrlencoded(formEl) {
+    const data = getFormData(formEl);
+    const params = buildQueryString(data);
+    const method = formEl.getAttribute('method');
+
+    const url = formEl.getAttribute('action');
+    const enctype = formEl.getAttribute('enctype') || 'application/x-www-form-urlencoded';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url + '?' + params);
+    xhr.setRequestHeader('Content-Type', enctype);
+    xhr.onreadystatechange = processResponse;
+    xhr.send();
+}
+
+
+function postUrlencoded(formEl) {
+    const data = getFormData(formEl);
+    const body = buildQueryString(data);
+    const method = formEl.getAttribute('method');
+
+    const url = formEl.getAttribute('action');
+    const enctype = formEl.getAttribute('enctype') || 'application/x-www-form-urlencoded';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader('Content-Type', enctype);
+    xhr.onreadystatechange = processResponse;
+    xhr.send(body);
+}
+
+function postMultipart(formEl) {
+    // construct body manually
+    const data = getFormData(formEl);
+    const url = formEl.getAttribute('action');
+
+    const multipartObj = buildMultipartObj(data);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + multipartObj.boundary);
+    xhr.onreadystatechange = processResponse;
+    xhr.send(multipartObj.body);
+}
+
+function postMultipartAlt(formEl) {
+    // XMLHttpRequest object supports FormData, no header required
+    const url = formEl.getAttribute('action');
+    const formData = new FormData(formEl);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.onreadystatechange = processResponse;
+    xhr.send(formData);
+}
+
+function postTextplain(formEl) {
+    const url = formEl.getAttribute('action');
+    const data = getFormData(formEl);
+
+    const textBody = Object.keys(data)
+        .map(el => el + '=' + data[el] + '\n')
+        .join('');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.onreadystatechange = processResponse;
+    xhr.send(textBody);
+}
+
+function postJson(formEl) {
+    const url = formEl.getAttribute('action');
+    const data = getFormData(formEl);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = processResponse;
+    xhr.send(JSON.stringify(data));
+}
+
+function postFiles(formEl) {
+    const url = formEl.getAttribute('action');
+    const formData = new FormData(formEl);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.onreadystatechange = processResponse;
+    xhr.send(formData);
+}
--- server.js
+++ server.js
@@ -1,61 +1,76 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const formData = require("express-form-data");
+const bodyParser = require('body-parser');
 
-var host = '0.0.0.0';
-var port = '7000';
+const hostname = 'localhost';
+const port = 7000;
 
-
-
-var myServer = http.createServer(function (req, res) {
-
-    console.log('requested ' + req.method + ' ' + req.url);
-
-    if (req.url.indexOf('/submit') !== -1) {
-        console.log('\033[41m', 'Submited!', '\033[0m');
-        console.log('\033[43m', 'Headers', '\033[0m', req.headers);
-        console.log('\033[44m', 'Body', '\033[0m');
-
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            console.log(body);
-            res.end('ok');
-        });
-
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(req.method + ' to ' + req.url);
-    } else if (req.url === '/' || req.url === '/index.html') {
-        fs.readFile('./static/index.html', (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    } else if (req.url === '/phones.json') {
-        setTimeout(function () {
-            fs.readFile('./static/phones.json', (err, data) => {
-                if (err) throw err;
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end(data);
-            });
-        }, 5000);
-    } else if (req.url === '/app.js') {
-        fs.readFile('./static/app.js', (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'application/javascript'});
-            res.end(data);
-        });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end(req.url + ' not found!');
-    }
+const app = express();
+app.use(morgan('dev')); // using morgan logger in 'dev' mode
+app.use(express.static(__dirname + '/public')); // serving static pages from folder
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(formData.parse({
+    autoFiles: true
+}));
 
 
+
+app.all('/submit/urlencoded', (req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/submit/urlencoded', (req, res, next) => {
+    // no body for GET request
+    console.log('no body!');
+    res.end('Success get urlencoded with no response body');
+});
+
+app.post('/submit/urlencoded', (req, res, next) => {
+    console.log(req.body);
+    res.end('Success post urlencoded: ' + '\r\n' + JSON.stringify(req.body));
+});
+
+app.post('/submit/multipart', (req, res, next) => {
+    console.log(req.body);
+    res.end('Success post multipart: ' + '\r\n' + JSON.stringify(req.body));
+
+});
+
+app.post('/submit/textplain', (req, res, next) => {
+    console.log(req.body);
+    res.end('Success post textplain: ' + '\r\n' + req.body);
+});
+
+app.post('/submit/json', (req, res, next) => {
+    console.log(req.body);
+    // even though HTML form has "application/json" encryption type, it uses default "urlencoded"
+    res.end('Success post JSON: ' + '\r\n' + JSON.stringify(req.body));
+});
+
+app.post('/submit/files', (req, res, next) => {
+    console.log(req.files);
+    const result = req.files.myUploadedFiles.map(el => {
+        return {
+            name: el.name,
+            type: el.type,
+            size: el.size
+        }
+    });
+    res.end('Success post a file: ' + '\r\n' + JSON.stringify(result));
 });
 
 
 
 
-myServer.listen(port, host);
-console.log('Server running at ' + host + ':' + port);
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}`);
+});
